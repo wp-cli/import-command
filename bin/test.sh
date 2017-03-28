2fd35ee16762c2b5ab59b6e47342c33040b2fd68
Daniel Bachhuber <daniel@handbuilt.co>
2017-03-28 11:58:09 -0700

    Newline test

--- bin/test.sh
+++ bin/test.sh
@@ -7,6 +7,7 @@ if [ $TRAVIS_COMMIT != $(git rev-parse HEAD) ]; then
 	exit 1
 fi
 
+
 # Run the functional tests
 BEHAT_TAGS=$(php utils/behat-tags.php)
 behat --format progress $BEHAT_TAGS --strict
