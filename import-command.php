872193c855274d051e3885ee9b672deeb32367e3
Alain Schlesser <alain.schlesser@gmail.com>
2019-04-17 14:56:29 +0200

    Prefix variables in global namespace

--- import-command.php
+++ import-command.php
@@ -4,9 +4,9 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
-$autoload = dirname( __FILE__ ) . '/vendor/autoload.php';
-if ( file_exists( $autoload ) ) {
-	require_once $autoload;
+$wpcli_import_autoloader = dirname( __FILE__ ) . '/vendor/autoload.php';
+if ( file_exists( $wpcli_import_autoloader ) ) {
+	require_once $wpcli_import_autoloader;
 }
 
 WP_CLI::add_command( 'import', 'Import_Command' );
